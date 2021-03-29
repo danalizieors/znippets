abf9ed4e68d8ea874631657a6a644dcd84dc9afc
Danalizi-Erőss Örs <danalizieors@gmail.com>
2021-03-30 02:15:22 +0300

    Avoid generating links with hyphens thus prevent line breaks

--- source/utilities/conversion.ts
+++ source/utilities/conversion.ts
@@ -4,11 +4,17 @@ import {
     strFromU8 as decodeUint8Array,
     strToU8 as encodeUint8Array,
 } from 'fflate'
-import { fromUint8Array, toUint8Array as decodeBase64 } from 'js-base64/base64'
+import { fromUint8Array, toUint8Array } from 'js-base64/base64'
+import { pipe, replace } from 'ramda'
+
+const removePadding = replace(/=/g, '')
+const encodeUrlSafe = pipe(replace(/\+/g, '~'), replace(/\//g, '_'))
+const decodeUrlSafe = pipe(replace(/~/g, '+'), replace(/_/g, '/'))
 
 const { stringify, parse } = JSON
 const compress = (data: Uint8Array) => deflateSync(data, { level: 9 })
-const encodeBase64 = (data: Uint8Array) => fromUint8Array(data, true)
+const encodeBase64 = pipe(fromUint8Array, encodeUrlSafe, removePadding)
+const decodeBase64 = pipe(decodeUrlSafe, toUint8Array)
 
 export const encode = (value: {} | null) => {
     const string = stringify(value)
