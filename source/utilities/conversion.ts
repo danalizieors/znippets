7f8d515d1d63fca7cc34e14943fe06c69de70a0c
Danalizi-Erőss Örs <danalizieors@gmail.com>
2021-03-31 18:38:56 +0300

    Make conversion functions more functional

--- source/utilities/conversion.ts
+++ source/utilities/conversion.ts
@@ -5,7 +5,7 @@ import {
     strToU8 as encodeUint8Array,
 } from 'fflate'
 import { fromUint8Array, toUint8Array } from 'js-base64/base64'
-import { pipe, replace } from 'ramda'
+import { always, pipe, replace, tryCatch } from 'ramda'
 
 const removePadding = replace(/=/g, '')
 const encodeUrlSafe = pipe(replace(/\+/g, '~'), replace(/\//g, '_'))
@@ -16,24 +16,14 @@ const compress = (data: Uint8Array) => deflateSync(data, { level: 9 })
 const encodeBase64 = pipe(fromUint8Array, encodeUrlSafe, removePadding)
 const decodeBase64 = pipe(decodeUrlSafe, toUint8Array)
 
-export const encode = (value: {} | null) => {
-    const string = stringify(value)
-    const uint8Array = encodeUint8Array(string)
-    const compressed = compress(uint8Array)
-    const base64 = encodeBase64(compressed)
+export const encode: (value: {} | null) => string = pipe(
+    stringify,
+    encodeUint8Array,
+    compress,
+    encodeBase64,
+)
 
-    return base64
-}
-
-export const decode = (base64: string) => {
-    try {
-        const compressed = decodeBase64(base64)
-        const uint8Array = decompress(compressed)
-        const string = decodeUint8Array(uint8Array)
-        const value = parse(string)
-
-        return value
-    } catch {
-        return undefined
-    }
-}
+export const decode: (base64: string) => any = tryCatch(
+    pipe(decodeBase64, decompress, decodeUint8Array, parse),
+    always(undefined),
+)
